49e4d07498f2f986208b3cb9b3187222925ba389
Naram Kim <unknown@grotesq.com>
2021-04-29 18:47:11 +0900

    Update install.ts

--- install.ts
+++ install.ts
@@ -10,5 +10,19 @@ async function getController( url : string ) {
     await download( url, destination )
 }
 
+async function appendRoutes() {
+    const dir = "./start/";
+    const encoder = new TextEncoder();
+    let text = "\n // Auth";
+    text += "\nRoute.post( '/auth/sign-up', 'AuthController.signUp' );";
+    text += "\nRoute.post( '/auth/sign-in', 'AuthController.signIn' );";
+    text += "\nRoute.get( '/me', 'UserController.getProfile' );";
+    text += "\nRoute.patch( '/me', 'UserController.updateProfile' );";
+    text += "\n";
+    const content = encoder.encode( text);
+    await Deno.writeFile( dir + "routes.js", content, { append: true } );
+}
+
 await getController("https://raw.githubusercontent.com/unk/adonis-auth-bootstrap/main/AuthController.js");
 await getController("https://raw.githubusercontent.com/unk/adonis-auth-bootstrap/main/UserController.js");
+await appendRoutes();
